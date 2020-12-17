cf12c312b68c82c4bdd49c018ebef1dab0db5a57
Null-Edge <nulledge.it@gmail.com>
2020-12-17 20:24:19 +0200

    Initial Files

---
+++ filters.js
@@ -0,0 +1,17 @@
+function DisplayImage(e)
+{
+    var imageFile = document.getElementById('imageFile');  
+    var canvas = document.getElementById('MainimageCanvas');
+    var ctx = canvas.getContext('2d');
+    var reader = new FileReader();
+    reader.onload = function(event){
+        var img = new Image();
+        img.onload = function(){
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);     
+}
